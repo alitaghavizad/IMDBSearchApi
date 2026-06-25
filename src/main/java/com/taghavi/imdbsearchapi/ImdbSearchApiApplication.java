140874ebe72409ac4e2b68ae18e3c5720a9a6909
AliTaghavi <alakak1376@gmail.com>
2026-06-25 08:31:06 +0330

    Init Commit

---
+++ src/main/java/com/taghavi/imdbsearchapi/ImdbSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.taghavi.imdbsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImdbSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taghavi/imdbsearchapi/ImdbSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taghavi.imdbsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbSearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
